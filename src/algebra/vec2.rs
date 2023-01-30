6741851cb4f1bdbf83672fc442cfa3e53e647c80
dhsu <=>
2023-01-30 13:20:05 +0100

    vec2

---
+++ src/algebra/vec2.rs
@@ -0,0 +1,8 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAs>
+use std::f32::consts::PI;
+ 
+#[derive(Copy, Clone, Debug)]
+struct Vector2 {
+    x: f32,
+    y: f32,
+}
